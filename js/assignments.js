586e3ba7cbe0e7eca3671ff3abcfbfd5e804c0c9
Malcolm Easton <eastonmalcolm@gmail.com>
2021-04-25 20:23:05 -0600

    assigned search button and added date rendering

--- js/assignments.js
+++ js/assignments.js
@@ -1,1 +1,7 @@
 //The purpose of this file is to make the final JQuery assignments to the document
+
+$('#search').click((e) => {
+    searchForCity($('#search-input').val());
+});
+
+renderDates();
