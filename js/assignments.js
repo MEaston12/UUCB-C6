05d50eb67777892cf806c41f06af901c276e6722
Malcolm Easton <eastonmalcolm@gmail.com>
2021-04-25 20:55:35 -0600

    added enter key functionality + loading

--- js/assignments.js
+++ js/assignments.js
@@ -4,4 +4,12 @@ $('#search').click((e) => {
     searchForCity($('#search-input').val() || 'Salt Lake City');
 });
 
+$(document).keypress((e) =>{
+    if(e.which == 13){
+        $('#search').click();
+    }
+});
+
+loadFromStorage();
 renderDates();
+renderCityButtons();
