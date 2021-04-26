35c3cefe01bd856078e4397adf0de0cb6433dc23
Malcolm Easton <eastonmalcolm@gmail.com>
2021-04-25 20:23:42 -0600

    defined render functions

--- js/functions.js
+++ js/functions.js
@@ -3,6 +3,7 @@ const weatherApiKey = '6364ff8c9b0798ae55417a9e7d6eba48';
 const weatherURL = `https://api.openweathermap.org/data/2.5/onecall?appid=${weatherApiKey}`;
 const googleApiKey = 'AIzaSyCCGMhhzz1pUJwb1-QP5lJQpll7SGi8wM8';
 const googleURL = `https://maps.googleapis.com/maps/api/geocode/json?key=${googleApiKey}`;
+const weatherInfoTable = {};
 
 async function getWeatherInfo(cityName){
     const googRes = await fetch(`${googleURL}&address=${encodeURIComponent(cityName)}`);
@@ -18,7 +19,65 @@ async function getWeatherInfo(cityName){
         alert('Error: ' + weatherRes.status);
         throw 'Weather API NO WORK';
     }
-    const weatherObj = await weatherRes.json();
-    //Might come back here and clean up the return object
-    return weatherObj;
+    const weatherInfo = await weatherRes.json();
+    const weatherOut = {
+        name: googObj.results[0].formatted_address,
+        days: [
+            {
+                temp: weatherInfo.current.temp,
+                wind: weatherInfo.current.wind_speed,
+                humidity: weatherInfo.current.humidity,
+                uvi: weatherInfo.current.uvi,
+                iconURL: `http://openweathermap.org/img/wn/${weatherInfo.current.weather[0].icon}@2x.png`
+            }
+        ]
+    }
+    for(let i = 1; i < 6; i++){
+        weatherOut.days[i] = {
+            temp: weatherInfo.daily[i-1].temp.day,
+            wind: weatherInfo.daily[i-1].wind_speed,
+            humidity: weatherInfo.daily[i-1].humidity,
+            iconURL: `http://openweathermap.org/img/wn/${weatherInfo.daily[i-1].weather[0].icon}@1x.png`
+        };
+    }
+    return weatherOut;
+}
+
+async function searchForCity(cityName){
+    const weatherInfo = await getWeatherInfo(cityName);
+    weatherInfoTable[weatherInfo.name] = weatherInfo;
+    renderCityButtons();
+    renderWeatherReadout(weatherInfo);
+}
+
+function renderCityButtons(){
+    $('#city-list').empty();
+    for(let cityName of Object.keys(weatherInfoTable)){
+        $('#city-list').append(`<button class='btn btn-secondary'>${cityName}</button>`);
+    }
+    $('#city-list').find('button').click((e) => {
+        renderWeatherReadout(weatherInfoTable[$(e.target).text()]);
+    });
+}
+
+function renderWeatherReadout(cityData){
+    const today = cityData.days[0];
+    $('#city-name').text(cityData.name);
+    $('#curr-temp').text(today.temp);
+    $('#curr-wind').text(today.wind);
+    $('#curr-humidity').text(today.humidity);
+    $('#curr-UV').text(today.uvi);
+}
+
+function dateToString(date){
+    return `${date.getMonth()+1}/${date.getDate()}/${date.getFullYear()}`;
+}
+
+function renderDates(){
+    const todayAndOtherDays = new Date();
+    $('#date-today').text(dateToString(todayAndOtherDays));
+    for(let i = 1; i < 6; i++){
+        todayAndOtherDays.setDate(todayAndOtherDays.getDate()+1);
+        $('#day-'+i).find('h4').text(dateToString(todayAndOtherDays));
+    }
 }
